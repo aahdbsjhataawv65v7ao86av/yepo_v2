9eee13dc1497b18d799943214954fbab82a7c263
NguyenNguyen12 <legumes_unicorn.8z@icloud.com>
2026-04-24 09:14:55 +0700

    added server

---
+++ server.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    // Determine the file path (defaults to index.html)
+    const filePath = path.join(__dirname, 'index.html');
+
+    // Read the file from the disk
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            res.writeHead(500);
+            res.end('Error loading index.html');
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content, 'utf-8');
+        }
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+});
